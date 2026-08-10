8486febe244554bc25d00b9a2ea0ae4ef392e021
Aditya kumar das <aditya.developer.it@gmail.com>
2026-08-11 00:44:30 +0530

    refactor: move app hook to hooks directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from "./components/layout/Navbar"
+import Home from "./pages/Home"
+
 function App() {
   return (
-    <div>
-      <h1>Smart Paw AI</h1>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+    </>
   )
 }
 
